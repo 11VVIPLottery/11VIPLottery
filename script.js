b51d4f47d51fc5180a700fd5694ee98894f4ac3a
11VVIPLottery <anshuluc51@gmail.com>
2026-02-17 15:21:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+let balance = 500;
 
+function updateBalance(){
+    const walletBoxes = document.querySelectorAll(".wallet-box");
+    walletBoxes.forEach(box=>{
+        box.innerText = "₹ " + balance.toFixed(2);
+    });
+}
+
+function recharge(){
+    let amount = prompt("Enter recharge amount:");
+    if(amount && amount > 0){
+        balance += parseInt(amount);
+        alert("Recharge Successful!");
+        updateBalance();
+    }
+}
+
+function withdraw(){
+    let amount = prompt("Enter withdraw amount:");
+    if(amount && amount <= balance){
+        balance -= parseInt(amount);
+        alert("Withdraw Successful!");
+        updateBalance();
+    } else {
+        alert("Insufficient Balance!");
+    }
+}
+
+function play(){
+    alert("Game Started!");
+}
+
+window.onload = updateBalance;
